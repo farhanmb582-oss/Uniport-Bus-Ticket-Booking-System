5804b6df9389e2cad305722786750dc1fe60efc9
farhanmb582 <farhanmb582@gmail.com>
2026-06-01 13:49:02 +0600

    Updated project files

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,13 +1,14 @@
-<nav>
-    <h2>Uniport</h2>
+<nav class="navbar">
+    <div class="logo">
+        🚌 Uniport
+    </div>
 
-    <ul>
+    <ul class="nav-links">
         <li><a href="index.php">Home</a></li>
         <li><a href="search.php">Search Bus</a></li>
         <li><a href="booking.php">Booking</a></li>
         <li><a href="history.php">History</a></li>
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
-        <li><a href="dashboard.php">Dashboard</a></li>
     </ul>
 </nav>
