defa29caa9824cc00702bde0cfb2643978464b17
farhanmb582 <farhanmb582@gmail.com>
2026-06-04 18:09:34 +0600

    Updated project files

--- edit.php
+++ edit.php
@@ -47,11 +47,24 @@ if(isset($_POST['update'])){
 
 <form method="POST">
 
-<input
-type="text"
-name="seat"
-value="<?php echo $row['seat_number']; ?>"
-required>
+<select name="seat" required>
+
+<option value="A1">A1</option>
+<option value="A2">A2</option>
+<option value="A3">A3</option>
+<option value="A4">A4</option>
+
+<option value="B1">B1</option>
+<option value="B2">B2</option>
+<option value="B3">B3</option>
+<option value="B4">B4</option>
+
+<option value="C1">C1</option>
+<option value="C2">C2</option>
+<option value="C3">C3</option>
+<option value="C4">C4</option>
+
+</select>
 
 <select name="status">
 
